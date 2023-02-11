ac2aa0c7cbd4d192225c4146af364f8396bc58c5
Александр Лёвин <maverick132@rambler.ru>
2023-02-12 01:18:02 +0300

    Method for add on RBTree

--- src/main/java/ru/geekbrains/lesson4/RedBlackTree.java
+++ src/main/java/ru/geekbrains/lesson4/RedBlackTree.java
@@ -7,6 +7,11 @@ public class RedBlackTree extends BinaryTree {
         root.isBlack = true;
     }
 
+    /**
+     * @param value Искомое значение
+     * @return Результат поиска элемента
+     */
+
     @Override
     public boolean exist(int value) {
         if (root != null) {
@@ -35,11 +40,66 @@ public class RedBlackTree extends BinaryTree {
         }
     }
 
+    public boolean add(int value) {
+        if (root == null) {
+            root.value = value;
+            return true;
+        } else {
+            Node node = root;
+            boolean flag = false;
+            while (!flag) {
+                if (node.value > value) {
+                    node = node.leftChild;
+                    if (node == null) {
+                        node.leftChild = new Node(value);
+                        flag = true;
+                    }
+                } else {
+                    node = node.rightChild;
+                    if (node == null) {
+                        node = new Node(value);
+                        flag = true;
+                    }
+                }
+            }
+            node.isBlack = false;
+
+        }
+
+        return false;
+    }
+
+    private Node findNode(Node node, int value) {
+        if (node.value > value) {
+            if (node.leftChild != null)
+                return find(node.leftChild, value);
+            else {
+                node.leftChild = new Node(value);
+                return node.leftChild;
+            }
+        } else if (node.rightChild != null)
+            return find(node.rightChild, value);
+        else {
+            node.rightChild = new Node(value);
+            return node.rightChild;
+
+        }
+    }
+
+
     private class Node {
         private int value;
         private Node leftChild;
         private Node rightChild;
         private boolean isBlack;
+
+        public Node() {
+            this(0);
+        }
+
+        public Node(int value) {
+            this.value = value;
+        }
     }
 
 
--- src/main/java/ru/geekbrains/lesson4/Sample01.java
+++ src/main/java/ru/geekbrains/lesson4/Sample01.java
@@ -7,22 +7,14 @@ import java.util.HashMap;
 public class Sample01 {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
+        RedBlackTree redBlackTree = new RedBlackTree();
+        redBlackTree.add(1);
+        redBlackTree.add(2);
+        redBlackTree.add(3);
+        redBlackTree.add(0);
+        redBlackTree.add(6);
 
-        HashMap<String, String> map = new HashMap<>();
-        map.put("abc", "Hello, GeekBrains!");
-        map.put("abc1", "Hello, GeekBrains!");
-        map.put("abc", "Hello, GeekBrains!");
 
-        MyHashTable<Integer, String> myTable = new MyHashTable<>();
-        myTable.put(1, "Value 1");
-        myTable.put(2, "Value 2");
-        myTable.put(3, "Value 3");
-        myTable.put(2, "Value 2");
-        myTable.remove(2);
-        String value1 = myTable.get(3);
-        String value2 = myTable.get(23);
     }
 
 }
