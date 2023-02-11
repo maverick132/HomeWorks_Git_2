f3ed004eae37c9b22f67fbc30b5cf0786099d1ef
Александр Лёвин <maverick132@rambler.ru>
2023-02-12 00:40:19 +0300

    Add RedBlackTree class and find method for BinaryTree

---
+++ src/main/java/ru/geekbrains/lesson4/RedBlackTree.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.lesson4;
+
+public class RedBlackTree extends BinaryTree {
+    private Node root;
+
+    {
+        root.isBlack = true;
+    }
+
+    @Override
+    public boolean exist(int value) {
+        if (root != null) {
+            Node node = find(root, value);
+            if (node != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Node find(Node node, int value) {
+        if (node.value == value) {
+            return node;
+        } else {
+            if (node.value > value) {
+                if (node.leftChild != null) {
+                    return find(node.leftChild, value);
+                } else return null;
+            } else {
+                if (node.rightChild != null) {
+                    return find(node.rightChild, value);
+                } else
+                    return null;
+            }
+        }
+    }
+
+    private class Node {
+        private int value;
+        private Node leftChild;
+        private Node rightChild;
+        private boolean isBlack;
+    }
+
+
+}
