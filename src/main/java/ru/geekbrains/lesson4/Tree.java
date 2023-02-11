c465d7372c5b46b6529cb5e85c92996210a1862f
Александр Лёвин <maverick132@rambler.ru>
2023-02-12 00:11:34 +0300

    Add find method

--- src/main/java/ru/geekbrains/lesson4/Tree.java
+++ src/main/java/ru/geekbrains/lesson4/Tree.java
@@ -4,7 +4,33 @@ import java.util.List;
 
 public class Tree {
 
-    public class Node{
+    Node root;
+
+    public boolean exist(int value) {
+        if (root != null) {
+            Node node = find(root, value);
+            if (node != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Node find(Node node, int value) {
+        if (node.value == value) {
+            return node;
+        } else {
+            for (Node child : node.children) {
+                Node result = find(child, value);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    public class Node {
         int value;
         List<Node> children;
     }
