946cc9807c16ea3fc66da165f3aeedb2a9bf1a40
Александр Лёвин <maverick132@rambler.ru>
2023-02-12 00:34:21 +0300

    Add find method for BinaryTree

--- src/main/java/ru/geekbrains/lesson4/BinaryTree.java
+++ src/main/java/ru/geekbrains/lesson4/BinaryTree.java
@@ -1,16 +1,41 @@
 package ru.geekbrains.lesson4;
 
-import java.util.List;
+public class BinaryTree extends Tree {
 
-public class BinaryTree extends Tree{
+    private Node root;
+    @Override
+    public boolean exist(int value) {
+        if (root != null) {
+            Node node = find(root, value);
+            if (node != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Node find(Node node, int value) {
+        if (node.value == value) {
+            return node;
+        } else {
+            if (node.value > value) {
+                if (node.leftChild != null) {
+                    return find(node.leftChild, value);
+                } else return null;
+            } else {
+                if (node.rightChild != null) {
+                    return find(node.rightChild, value);
+                } else
+                    return null;
+            }
+        }
+    }
 
     private class Node {
         private int value;
         private Node leftChild;
         private Node rightChild;
     }
-
-
 
 
 }
